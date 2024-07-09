4db2f0af0080ccfaaf956754cf43de38f6247b5d
Ji Chen <jaykchen@gmail.com>
2024-07-09 15:48:27 -0400

    build without sveltekit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({ plugins: [svelte()] });
+export default defineConfig({
+    plugins: [svelte()],
+    build: {
+        minify: false,
+    },
+});
