6c0411beb0412d657668a59a3d0f7ad5966cc949
Ji Chen <jaykchen@gmail.com>
2024-07-10 14:42:41 -0400

    can use daisyUI components tree-shake css cssnano-advanced aggressive opt

---
+++ command.sh
@@ -0,0 +1,6 @@
+
+npm i -D daisyui@latest
+npm install tailwindcss postcss autoprefixer daisyui
+npx tailwindcss init
+npm install cssnano cssnano-preset-advanced --save-dev
+npm install cssnano --save-dev
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,43 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-    plugins: [svelte()],
-    build: {
-        minify: false,
+  plugins: [svelte()],
+  build: {
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      treeshake: {
+        // moduleSideEffects: [
+        //   "tailwindcss",
+        //   "cssnano",
+        //   "postcss",
+        //   "cssnano-preset-advanced",
+        // ], // Assume no side effects in modules
+        // moduleSideEffects: "some", // Assume no side effects in modules
+        propertyReadSideEffects: false, // Ignore property read side effects
+        tryCatchDeoptimization: false, // Ignore try-catch deoptimizations
+      },
     },
+    commonjsOptions: {
+      include: /node_modules/,
+      transformMixedEsModules: true,
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ["console.log"], // Remove console.log statements
+        passes: 3, // Apply multiple passes for better optimization.
+      },
+      mangle: { toplevel: true }, // Mangle variable names for smaller output.
+      output: {
+        comments: false, // Remove comments in the output files.
+      },
+    },
+  },
+  optimizeDeps: {
+    // Disable dependency pre-bundling
+    disabled: true,
+  },
 });
